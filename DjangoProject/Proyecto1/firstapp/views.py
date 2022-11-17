ad57bd89a2629b867dd97d3a5a6196859acc3f16
UnknownIsaac <isaacsims100@gmail.com>
2022-11-17 19:11:11 +0100

    django dont know

--- DjangoProject/Proyecto1/firstapp/views.py
+++ DjangoProject/Proyecto1/firstapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 #Create your views here
 
 def index(request):
-    return HttpResponse("Hola Mundo")
+    return HttpResponse("Hello, world. You're at the polls index.")
