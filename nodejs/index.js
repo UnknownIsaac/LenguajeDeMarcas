d115be79801aa687e06700035b58c6cb4ec45664
UnknownIsaac <isaacsims100@gmail.com>
2023-02-17 19:16:04 +0100

    node

--- nodejs/index.js
+++ nodejs/index.js
@@ -4,22 +4,29 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs")
+
+const libros = [
+    {
+        nombre: "Libro1",
+        isbn: "63463213213"
+    },
+    {
+        nombre: "Libro2",
+        isbn: "63463213213"
+    },
+];
+
 app.get("/", (req, res) => {
     res.send("Hola mundo")
 });
 
+app.get("/libreria", (req, res) => {
+    res.render("home", { libros: libros});
+})
+
 app.get("/libros", (req, res) => {
     // llamada a BBDD
-    const libros = [
-        {
-            nombre: "Libro1",
-            isbn: "63463213213"
-        },
-        {
-            nombre: "Libro2",
-            isbn: "63463213213"
-        },
-    ];
 
     res.setHeader("Content-type", "application/json");
     res.json(libros);
